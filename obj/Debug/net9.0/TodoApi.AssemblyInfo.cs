266365b875720a2233b7219509e3f976237c95de
DanielOkoloSanchez <dokolosfp1@gmail.com>
2025-01-19 12:18:22 +0100

    no se

--- obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0620883054b39d22b5589e159acc9df5589e6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ef1329e51cedb9627544ed35cfd4fe991ad2018")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
